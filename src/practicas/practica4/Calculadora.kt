9934b72c7298c13613c928090c86f732ea908d51
Adrian <obsidianagf@gmail.com>
2026-09-18 17:51:03 +0200

    18/09/2026 - Practica4 : Calculadora     - Refactor-> practicas.practica4

---
+++ src/practicas/practica4/Calculadora.kt
@@ -0,0 +1,18 @@
+package practicas.practica4
+
+class Calculadora {
+
+
+    fun sumar(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun resta(a: Int, b: Int): Int {
+        return a-b
+    }
+    fun multiplicacion(a: Int, b: Int): Int {
+        return a*b
+    }
+    fun division(a: Int, b: Int): Int {
+        return a/b
+    }
+}
